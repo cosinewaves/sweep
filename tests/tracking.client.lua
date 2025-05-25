6187068288381666e45ec01e89dc4a29fb7512bc
cosinewaves <elkhaadnan@gmail.com>
2025-05-25 20:35:05 +0100

    test

--- tests/tracking.client.lua
+++ tests/tracking.client.lua
@@ -2,6 +2,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local sweep = require(ReplicatedStorage.sweep)
 
 local collector = sweep.new()
+print("Created new sweep class")
 
 function CreatePart(): Part
   local new = Instance.new("Part")
@@ -9,8 +10,11 @@ function CreatePart(): Part
   return new
 end
 
+print("Creating parts")
 for i = 1, 10, 1 do
   collector:Track(CreatePart())
 end
+print("Created parts")
 
+print("destorying ")
 task.delay(5, collector.Wipe, collector)
