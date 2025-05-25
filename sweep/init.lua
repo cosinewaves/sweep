7655477563b64b59c616e8bce1c4c6782e18de19
cosinewaves <elkhaadnan@gmail.com>
2025-05-25 20:06:04 +0100

    create init.lua

--- sweep/init.lua
+++ sweep/init.lua
@@ -1,2 +1,10 @@
 --!strict
 -- init.lua
+
+local sweeper = require(script.sweeper :: ModuleScript)
+local internalTypings = require(script.internalTypings :: ModuleScript)
+
+export type sweeperTask = internalTypings.sweeperTask
+export type sweeper = internalTypings.sweeper
+
+return sweeper
