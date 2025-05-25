0cca34195712b7556f035be5fa42cb4548c259a3
cosinewaves <elkhaadnan@gmail.com>
2025-05-25 21:42:11 +0100

    fix

--- sweep/internalTypings.lua
+++ sweep/internalTypings.lua
@@ -1,29 +1,33 @@
---!strict
--- internalTypings.lua
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local sweep = require(ReplicatedStorage.sweep)
 
-export type sweeperTask =
-	Instance
-	| RBXScriptConnection
-	| () -> ...any
-	| {
-		Destroy: (self: any) -> nil
-	}
-	| sweeper -- allow nested sweeper
+-- Utility to create a test part
+function CreatePart(): Part
+	local new = Instance.new("Part")
+	new.Name = "SweeperPart"
+	new.Parent = workspace
+	return new
+end
 
-export type bucketItem = {
-	task: sweeperTask,
-	tag: string?,
-}
+print("Creating parent and child sweepers")
+local parent = sweep.new()
+local child = sweep.new()
 
-export type sweeper = {
-	_bucket: { bucketItem },
+-- Track the child in the parent
+parent:Track(child)
 
-	Track: (self: sweeper, task: sweeperTask, tag: string?) -> sweeperTask,
-	Wipe: (self: sweeper, tag: string?) -> sweeper,
+-- Add some parts to the child
+print("Adding parts to child sweeper")
+for i = 1, 5 do
+	child:Track(CreatePart())
+end
 
-	OnTrack: (task: sweeperTask) -> (),
-	OnWipe: () -> (),
+print("Adding parts to parent sweeper")
+for i = 1, 5 do
+	parent:Track(CreatePart())
+end
 
-}
-
-return {}
+print("Delaying 5 seconds before wiping parent (which should also wipe child)")
+task.delay(5, function()
+	parent:Wipe()
+end)
