3d516d9f4a691c1ee4c2a0b047186bbf242fbde8
cosinewaves <elkhaadnan@gmail.com>
2025-05-25 20:31:20 +0100

    add tests

--- tests/tracking.client.lua
+++ tests/tracking.client.lua
@@ -1,2 +1,16 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local sweep = require(ReplicatedStorage.sweep)
+
+local collector = sweep.new()
+
+function CreatePart(): Part
+  local new = Instance.new("Part")
+  new.Parent = workspace
+  return new
+end
+
+for i = 1, 10, 1 do
+  collector:Track(CreatePart())
+end
+
+task.delay(5, collector.Wipe, collector)
